d1da3f864515282ce9b173bfc29f12ea4c53039c
trba1810 <trba8l@live.com>
2023-02-11 23:43:24 +0100

    added MenuItems controller and getall action

---
+++ RedMango_API/Controllers/MenuItemController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using RedMango_API.Data;
+using RedMango_API.Models;
+using System.Net;
+
+namespace RedMango_API.Controllers
+{
+    [Route("api/MenuItem")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private ApiResponse _response;
+
+        public MenuItemController(ApplicationDbContext context)
+        {
+            _context = context;
+            _response = new ApiResponse(); 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMenuItems()
+        {
+            _response.Result = _context.MenuItemUsers;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+    }
+}
