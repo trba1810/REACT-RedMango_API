95aee25b2fb854323b278e95fdf80db8872e8f7e
trba1810 <trba8l@live.com>
2023-02-16 20:27:12 +0100

    added user registeration and auth

---
+++ RedMango_API/Controllers/AuthController.cs
@@ -0,0 +1,86 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RedMango_API.Data;
+using RedMango_API.Models;
+using RedMango_API.Models.DTO;
+using RedMango_API.Utility;
+using System.Net;
+
+namespace RedMango_API.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private ApiResponse _response;
+        private string secretKey;
+
+        public AuthController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment, IConfiguration configuration,
+            UserManager<ApplicationUser> userManager,RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+            _response = new ApiResponse();
+            _webHostEnvironment = webHostEnvironment;
+            secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDTO model)
+        {
+            ApplicationUser userFromDB = _context.ApplicationUsers.FirstOrDefault(x => x.UserName.ToLower() == model.UserName.ToLower());
+            if (userFromDB != null)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add("Username already exist");
+                return BadRequest(_response);
+            }
+            ApplicationUser newUser = new()
+            {
+                UserName = model.UserName,
+                Email = model.UserName,
+                NormalizedEmail = model.UserName.ToUpper(),
+                Name = model.Name,
+            };
+            try { 
+            var result = await _userManager.CreateAsync(newUser, model.Password);
+                if (result.Succeeded)
+                {
+                    if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin));
+                        await _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer));
+                    }
+
+                    if (model.Role.ToLower() == SD.Role_Admin)
+                    {
+                        await _userManager.AddToRoleAsync(newUser, SD.Role_Admin);
+                    }
+                    else
+                    {
+                        await _userManager.AddToRoleAsync(newUser, SD.Role_Customer);
+                    }
+                    _response.StatusCode = HttpStatusCode.OK;
+                    _response.IsSuccess = true;
+                    return Ok(_response);
+                }
+            }
+            catch(Exception)
+            {
+
+            }
+            _response.StatusCode = HttpStatusCode.BadRequest;
+            _response.IsSuccess = false;
+            _response.ErrorMessages.Add("Error with registering");
+            return BadRequest(_response);
+        }
+    }
+}
