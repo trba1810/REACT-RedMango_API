5c4feb4ba508c3942615540b2adacf3d8d847483
trba1810 <trba8l@live.com>
2023-02-20 23:31:55 +0100

    added get shopping cart endpoint

--- RedMango_API/Controllers/ShoppingCartController.cs
+++ RedMango_API/Controllers/ShoppingCartController.cs
@@ -20,6 +20,35 @@ namespace RedMango_API.Controllers
             _context = context;
         }
 
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse>> GetShoppingCart (string userId)
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(userId))
+                {
+                    _response.IsSuccess= false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+                }
+                ShoppingCart shoppingCart = _context.ShoppingCarts.Include(x => x.CartItems).ThenInclude(x => x.MenuItem).FirstOrDefault(x => x.UserId == userId);
+                if(shoppingCart.CartItems != null && shoppingCart.CartItems.Count > 0)
+                {
+                    shoppingCart.CartTotal = shoppingCart.CartItems.Sum(x => x.quantity * x.MenuItem.Price);
+                }
+                _response.Result = shoppingCart;
+                _response.StatusCode=HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch(Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString()};
+                _response.StatusCode = HttpStatusCode.BadRequest;
+            }
+            return _response;
+        } 
+
         [HttpPost]
         public async Task<ActionResult<ApiResponse>> AddorUpdateItemInCart(string userId,int menuItemId,int updateQuantityBy)
         {
