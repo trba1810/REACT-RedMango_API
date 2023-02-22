024787002fc2533600c7f92a80897695ef06732c
trba1810 <trba8l@live.com>
2023-02-22 05:04:38 +0100

    create order action

--- RedMango_API/Controllers/OrderController.cs
+++ RedMango_API/Controllers/OrderController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RedMango_API.Data;
 using RedMango_API.Models;
+using RedMango_API.Models.DTO;
+using RedMango_API.Utility;
 using System.Net;
 
 namespace RedMango_API.Controllers
@@ -67,6 +69,54 @@ namespace RedMango_API.Controllers
                 _response.Result = orderHeaders;
                 _response.StatusCode = HttpStatusCode.OK;
                 return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> CreateOrder([FromBody] OrderHeaderCreateDTO orderHeaderDTO)
+        {
+            try
+            {
+                OrderHeader order = new()
+                {
+                    ApplicationUserId = orderHeaderDTO.ApplicationUserId,
+                    PickupEmail = orderHeaderDTO.PickupEmail,
+                    PickupName = orderHeaderDTO.PickupName,
+                    PickupPhoneNumber = orderHeaderDTO.PickupPhoneNumber,
+                    OrderTotal = orderHeaderDTO.OrderTotal,
+                    OrderDate = DateTime.Now,
+                    StripePaymentIntentId = orderHeaderDTO.StripePaymentIntentId,
+                    TotalItems = orderHeaderDTO.TotalItems,
+                    Status = String.IsNullOrEmpty(orderHeaderDTO.Status) ? SD.status_pending : orderHeaderDTO.Status,
+                };
+                if(ModelState.IsValid )
+                {
+                    _context.OrderHeaders.Add(order);
+                    _context.SaveChanges();
+                    foreach(var orderDetailDTO in orderHeaderDTO.OrderDetailsDTO)
+                    {
+                        OrderDetails orderDetails = new()
+                        {
+                            OrderHeaderId = order.OrderHeaderId,
+                            ItemName = orderDetailDTO.ItemName,
+                            MenuItemId = orderDetailDTO.MenuItemId,
+                            Price = orderDetailDTO.Price,
+                            Quantity = orderDetailDTO.Quantity,
+                        };
+                        _context.OrderDetails.Add(orderDetails);
+                    }
+                    _context.SaveChanges();
+                    _response.Result = order;
+                    order.OrderDetails = null;
+                    _response.StatusCode = HttpStatusCode.Created;
+                    return Ok(_response);
+                }
             }
             catch (Exception ex)
             {
--- RedMango_API/Utility/SD.cs
+++ RedMango_API/Utility/SD.cs
@@ -4,5 +4,12 @@
     {
         public const string Role_Admin = "admin";
         public const string Role_Customer = "customer";
+
+        public const string status_pending = "Pending";
+        public const string status_confirmed = "Confirmed";
+        public const string status_beingCooked = "Being Cooked";
+        public const string status_readyForPickUp = "Ready for PickUp";
+        public const string status_completed = "Completed";
+        public const string status_cancelled = "Cancelled";
     }
 }
