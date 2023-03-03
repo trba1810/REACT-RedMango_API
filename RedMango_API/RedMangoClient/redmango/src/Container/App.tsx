faa40f015557938785046096196bd91061c6fa6f
trba1810 <trba8l@live.com>
2023-03-03 07:30:40 +0100

    install react route dom and initialize home page

--- RedMango_API/RedMangoClient/redmango/src/Container/App.tsx
+++ RedMango_API/RedMangoClient/redmango/src/Container/App.tsx
@@ -3,12 +3,17 @@ import { Header, Footer } from "../Components/Layout";
 import { useState, useEffect } from "react";
 import { menuItemModel } from "../Interfaces";
 import { Home } from "../Pages";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header />
-      <Home />
+      <div className="pb-5">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
