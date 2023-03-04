94ddff7280e7b11e0efe30c689ed77fe6fc8c2c0
trba1810 <trba8l@live.com>
2023-03-04 07:33:03 +0100

    configure redux store

---
+++ RedMango_API/RedMangoClient/redmango/src/Storage/Redux/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit/dist/configureStore";
+
+import { menuItemReducer } from "./menuItemSlice";
+
+const store = configureStore({
+  reducer: {
+    menuItemStore: menuItemReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export default store;
