0b0d97aa6fb846a11c4316373dacf8ed1e465c17
trba1810 <trba8l@live.com>
2023-03-04 06:52:19 +0100

    added menu item details page

--- RedMango_API/RedMangoClient/redmango/src/Container/App.tsx
+++ RedMango_API/RedMangoClient/redmango/src/Container/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Header, Footer } from "../Components/Layout";
 import { useState, useEffect } from "react";
 import { menuItemModel } from "../Interfaces";
-import { Home, NotFound } from "../Pages";
+import { Home, MenuItemDetails, NotFound } from "../Pages";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -12,6 +12,10 @@ function App() {
       <div className="pb-5">
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route
+            path="/menuItemDetails/:menuItemId"
+            element={<MenuItemDetails />}
+          ></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </div>
