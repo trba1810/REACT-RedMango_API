62f446da7eee8a93fcce9100e769b3a4870659eb
trba1810 <trba8l@live.com>
2023-03-06 10:15:33 +0100

    call slice

--- RedMango_API/RedMangoClient/redmango/src/Components/Page/MenuItems/MenuItemList.tsx
+++ RedMango_API/RedMangoClient/redmango/src/Components/Page/MenuItems/MenuItemList.tsx
@@ -1,23 +1,30 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useGetMenuItemsQuery } from "../../../Apis/menuItemApi";
 import { menuItemModel } from "../../../Interfaces";
+import { setMenuItem } from "../../../Storage/Redux/menuItemSlice";
 import MenuItemCard from "./MenuItemCard";
 
 function MenuItemList() {
-  const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+  //const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+  const dispatch = useDispatch();
+  const { data, isLoading } = useGetMenuItemsQuery(null);
 
   useEffect(() => {
-    fetch("")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setMenuItems(data.result);
-      });
-  }, []);
+    if (!isLoading) {
+      dispatch(setMenuItem(data.result));
+    }
+  }, [isLoading]);
+
+  if (isLoading) {
+    return <div>isLoading</div>;
+  }
+
   return (
     <div className="container row">
-      {menuItems.length > 0 &&
-        menuItems.map((menuItem, index) => (
+      {data.result.length > 0 &&
+        data.result.map((menuItem: menuItemModel, index: number) => (
           <MenuItemCard menuItem={menuItem} key={index} />
         ))}{" "}
     </div>
