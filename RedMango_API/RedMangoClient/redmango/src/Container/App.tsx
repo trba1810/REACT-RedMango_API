4faf6cb0a00db0e6bb44a887ebb56098b3fc06cb
trba1810 <trba8l@live.com>
2023-03-11 18:34:06 +0100

    add shopping cart slice to store

--- RedMango_API/RedMangoClient/redmango/src/Container/App.tsx
+++ RedMango_API/RedMangoClient/redmango/src/Container/App.tsx
@@ -4,8 +4,22 @@ import { useState, useEffect } from "react";
 import { menuItemModel } from "../Interfaces";
 import { Home, MenuItemDetails, NotFound } from "../Pages";
 import { Route, Routes } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useGetShoppingCartQuery } from "../Apis/shoppingCartApi";
+import { setShoppingCart } from "../Storage/Redux/shoppingCartSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const { data, isLoading } = useGetShoppingCartQuery(
+    "a79aa4b7-cd94-4b80-af14-b1d7c98b4c10"
+  );
+  useEffect(() => {
+    if (!isLoading) {
+      console.log(data.result);
+      dispatch(setShoppingCart(data.result?.cartItems));
+    }
+  }, [data]);
+
   return (
     <div>
       <Header />
