fc6984f14176cbf0588bbb3e994137128edd2271
trba1810 <trba8l@live.com>
2023-03-12 14:05:01 +0100

    added shopping cart reducers

--- RedMango_API/RedMangoClient/redmango/src/Storage/Redux/shoppingCartSlice.ts
+++ RedMango_API/RedMangoClient/redmango/src/Storage/Redux/shoppingCartSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { shoppingCartModel } from "../../Interfaces";
 
-const initialState = {
+const initialState: shoppingCartModel = {
   cartItems: [],
 };
 
@@ -11,8 +12,26 @@ export const shoppingCartSlice = createSlice({
     setShoppingCart: (state, action) => {
       state.cartItems = action.payload;
     },
+    updateQuantity: (state, action) => {
+      state.cartItems = state.cartItems?.map((item) => {
+        if (item.id === action.payload.cartItem.id) {
+          item.quantity = action.payload.quantity;
+        }
+        return item;
+      });
+    },
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems?.filter((item) => {
+        if (item.id === action.payload.cartItem.id) {
+          item.quantity = action.payload.quantity;
+          return null;
+        }
+        return item;
+      });
+    },
   },
 });
 
-export const { setShoppingCart } = shoppingCartSlice.actions;
+export const { setShoppingCart, updateQuantity, removeFromCart } =
+  shoppingCartSlice.actions;
 export const shoppingCartReducer = shoppingCartSlice.reducer;
