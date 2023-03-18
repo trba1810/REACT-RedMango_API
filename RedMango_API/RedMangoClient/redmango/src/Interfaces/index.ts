6f0fb27a7b600bb89bfc834724935a5f84466a58
trba1810 <trba8l@live.com>
2023-03-18 12:17:05 +0100

    added response api interface

--- RedMango_API/RedMangoClient/redmango/src/Interfaces/index.ts
+++ RedMango_API/RedMangoClient/redmango/src/Interfaces/index.ts
@@ -2,5 +2,11 @@ import menuItemModel from "./menuItemModel";
 import shoppingCartModel from "./shoppingCartModel";
 import cartItemModel from "./cartItemModel";
 import userModel from "./userModel";
-
-export type { menuItemModel, shoppingCartModel, cartItemModel, userModel };
+import apiResponse from "./apiResponse";
+export type {
+  menuItemModel,
+  shoppingCartModel,
+  cartItemModel,
+  userModel,
+  apiResponse,
+};
