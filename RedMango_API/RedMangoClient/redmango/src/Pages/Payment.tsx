be82b667dd7ad3358143c029d68cd7854c1654d1
trba1810 <trba8l@live.com>
2023-04-07 16:13:02 +0200

    added card payment form

--- RedMango_API/RedMangoClient/redmango/src/Pages/Payment.tsx
+++ RedMango_API/RedMangoClient/redmango/src/Pages/Payment.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { PaymentForm } from "../Components/Page/Payment";
 
 function Payment() {
   const {
     state: { apiResult, userInput },
   } = useLocation();
-  console.log(apiResult);
-  console.log(userInput);
-  return <div>Payment</div>;
+  const stripePromise = loadStripe(
+    "pk_test_51MenYIHluzbSyBfpTPaP6VDy8ZTmLDNfRjLjLNqNPxR5XcOEvBpMU9dREA0qjZ911E9V5LOGoxVdgSdkRC1qEFQ300R4aFcFQ5"
+  );
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: apiResult.clientSecret,
+  };
+
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <PaymentForm />
+    </Elements>
+  );
 }
 
 export default Payment;
