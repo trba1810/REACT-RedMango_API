a47d63e9ee0f669bd130312179f56165c99d567d
trba1810 <trba8l@live.com>
2023-04-19 23:05:54 +0200

    dynamic my orders

--- RedMango_API/RedMangoClient/redmango/src/Interfaces/index.ts
+++ RedMango_API/RedMangoClient/redmango/src/Interfaces/index.ts
@@ -3,10 +3,14 @@ import cartItemModel from "./cartItemModel";
 import shoppingCartModel from "./shoppingCartModel";
 import userModel from "./userModel";
 import apiResponse from "./apiResponse";
+import orderDetail from "./orderDetail";
+import orderHeader from "./orderHeader";
 export type {
   menuItemModel,
   cartItemModel,
   shoppingCartModel,
   userModel,
   apiResponse,
+  orderDetail,
+  orderHeader,
 };
