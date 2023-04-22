67059d3f316e7d179bb4fa8153fd6248a2c9f230
trba1810 <trba8l@live.com>
2023-04-22 17:10:19 +0200

    button for next status

--- RedMango_API/RedMangoClient/redmango/src/Components/Page/Order/OrderSummary.tsx
+++ RedMango_API/RedMangoClient/redmango/src/Components/Page/Order/OrderSummary.tsx
@@ -3,10 +3,24 @@ import { orderSummaryProps } from "./orderSummaryProps";
 import { cartItemModel } from "../../../Interfaces";
 import { getStatusColor } from "../../../Helper";
 import { useNavigate } from "react-router-dom";
+import { SD_Roles, SD_Status } from "../../../Utility/SD";
+import { RootState } from "../../../Storage/Redux/store";
+import { useSelector } from "react-redux";
 
 function OrderSummary({ data, userInput }: orderSummaryProps) {
   const badgeTypeColor = getStatusColor(data.status!);
   const navigate = useNavigate();
+  const userData = useSelector((state: RootState) => state.userAuthStore);
+
+  const nextStatus: any =
+    data.status! === SD_Status.CONFIRMED
+      ? { color: "info", value: SD_Status.BEING_COOKED }
+      : data.status! === SD_Status.BEING_COOKED
+      ? { color: "warning", value: SD_Status.READY_FOR_PICKUP }
+      : data.status! === SD_Status.READY_FOR_PICKUP && {
+          color: "success",
+          value: SD_Status.COMPLETED,
+        };
   return (
     <div>
       {" "}
@@ -51,6 +65,14 @@ function OrderSummary({ data, userInput }: orderSummaryProps) {
         <button className="btn btn-secondary" onClick={() => navigate(-1)}>
           Back to Orders
         </button>
+        {userData.role == SD_Roles.ADMIN && (
+          <div className="d-flex">
+            <button className="btn btn-danger mx-2">Cancel</button>
+            <button className={`btn btn-${nextStatus.color}`}>
+              {nextStatus.value}
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
