ccb4f4fc0393a984fc0b888a886772b180464d12
trba1810 <trba8l@live.com>
2023-05-05 16:54:09 +0200

    Local Filters in Action

--- RedMango_API/RedMangoClient/redmango/src/Pages/Order/AllOrders.tsx
+++ RedMango_API/RedMangoClient/redmango/src/Pages/Order/AllOrders.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { withAdminAuth, withAuth } from "../../HOC";
 import { useSelector } from "react-redux";
 import { RootState } from "../../Storage/Redux/store";
@@ -7,6 +7,7 @@ import OrderList from "../../Components/Page/Order/OrderList";
 import { MainLoader } from "../../Components/Page/Common";
 import { inputHelper } from "../../Helper";
 import { SD_Status } from "../../Utility/SD";
+import { orderHeaderModel } from "../../Interfaces";
 const filterOptions = [
   "All",
   SD_Status.CONFIRMED,
@@ -18,6 +19,7 @@ const filterOptions = [
 function AllOrders() {
   const { data, isLoading } = useGetAllOrdersQuery("");
   const [filters, setFilters] = useState({ searchString: "", status: "" });
+  const [orderData, setOrderData] = useState([]);
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
@@ -25,6 +27,34 @@ function AllOrders() {
     const tempValue = inputHelper(e, filters);
     setFilters(tempValue);
   };
+
+  const handleFilters = () => {
+    const tempData = data.result.filter((orderData: orderHeaderModel) => {
+      if (
+        (orderData.pickupName &&
+          orderData.pickupName.includes(filters.searchString)) ||
+        (orderData.pickupEmail &&
+          orderData.pickupEmail.includes(filters.searchString)) ||
+        (orderData.pickupPhoneNumber &&
+          orderData.pickupPhoneNumber.includes(filters.searchString))
+      ) {
+        return orderData;
+      }
+    });
+
+    const finalArray = tempData.filter((orderData: orderHeaderModel) =>
+      filters.status !== "" ? orderData.status === filters.status : orderData
+    );
+
+    setOrderData(finalArray);
+  };
+
+  useEffect(() => {
+    if (data) {
+      setOrderData(data.result);
+    }
+  }, [data]);
+
   return (
     <>
       {isLoading && <MainLoader />}
@@ -45,15 +75,22 @@ function AllOrders() {
                 onChange={handleChange}
                 name="status"
               >
-                {filterOptions.map((item) => (
-                  <option value={item === "All" ? "" : item}>{item}</option>
+                {filterOptions.map((item, index) => (
+                  <option key={index} value={item === "All" ? "" : item}>
+                    {item}
+                  </option>
                 ))}
               </select>
-              <button className="btn btn-outline-success">Filter</button>
+              <button
+                className="btn btn-outline-success"
+                onClick={handleFilters}
+              >
+                Filter
+              </button>
             </div>
           </div>
 
-          <OrderList isLoading={isLoading} orderData={data.result} />
+          <OrderList isLoading={isLoading} orderData={orderData} />
         </>
       )}
     </>
